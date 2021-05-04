42ad477d74c3da05a03f3969bc2f222865477975
suiweidong <7334501@qq.com>
2021-05-04 22:09:08 +0800

    mall-admin

--- mall-admin/src/main/java/com/study/code/MallAdminApplication.java
+++ mall-admin/src/main/java/com/study/code/MallAdminApplication.java
@@ -13,10 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class RenrenApplication {
+public class MallAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RenrenApplication.class, args);
+		SpringApplication.run(MallAdminApplication.class, args);
 	}
 
 }
