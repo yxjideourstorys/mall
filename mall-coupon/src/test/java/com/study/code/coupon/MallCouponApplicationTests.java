ed3b8af9d303c9be949d43d9c1de71a7bd3bd6b5
suiweidong <7334501@qq.com>
2021-05-04 22:06:15 +0800

    mall-admin

---
+++ mall-coupon/src/test/java/com/study/code/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.code.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-member/src/test/java/com/study/code/member/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.code.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
