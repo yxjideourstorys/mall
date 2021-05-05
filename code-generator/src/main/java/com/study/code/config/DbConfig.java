c0219a5aed38b87a1dfd66ec2dfc60550938e74e
suiweidong <7334501@qq.com>
2021-05-06 01:52:04 +0800

    1

--- code-generator/src/main/java/com/study/code/config/DbConfig.java
+++ code-generator/src/main/java/com/study/code/config/DbConfig.java
@@ -10,7 +10,6 @@ package com.study.code.config;
 
 import com.study.code.dao.*;
 import com.study.code.utils.RRException;
-import io.renren.dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
