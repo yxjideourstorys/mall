a9b7533061713477ed13b5ee9c75683f4ea80f6b
suiweidong <7334501@qq.com>
2021-05-11 10:54:36 +0800

    spring cloud alibaba:  nacos 服务及config配置

--- mall-product/src/main/java/com/study/code/product/controller/FeignTestController.java
+++ mall-product/src/main/java/com/study/code/product/controller/FeignTestController.java
@@ -3,9 +3,12 @@ package com.study.code.product.controller;
 import com.study.code.commons.feign.coupon.CouponFeignService;
 import com.study.code.utils.util.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @RequestMapping("/diaofeign")
 public class FeignTestController {
@@ -13,8 +16,16 @@ public class FeignTestController {
     @Autowired
     private CouponFeignService couponFeignService;
 
+    @Value("${dgg.info}")
+    private String info;
+
     @RequestMapping("/test")
     public R test(){
         return this.couponFeignService.test();
+    }
+
+    @RequestMapping("/info")
+    public R info(){
+        return R.ok(info);
     }
 }
